159a848d91ffa647c5b26c02a27b616cf48b75a6
Edgar Perez <edgarjoel9912@gmail.com>
2025-06-05 22:20:50 -0400

    Cambio de nombre al home page del kanban

--- src/pages/KanbanPage.tsx
+++ src/pages/KanbanPage.tsx
@@ -8,7 +8,7 @@ export default function KanbanPage() {
       <main className="flex flex-col h-screen overflow-hidden w-full md:pl-[4rem]">
         {/* Encabezado del Kanban */}
         <header className="px-4 md:px-6 lg:px-8 pb-0 pt-20 md:pt-8">
-          <h2 className="text-3xl font-bold">Tablero Kanban - Mantenimiento</h2>
+          <h2 className="text-3xl font-bold">Tablero</h2>
         </header>
         {/* El área del tablero se expande, el scroll es SOLO horizontal */}
         <section className="flex-1 overflow-x-auto overflow-y-hidden px-4 md:px-6 lg:px-8 pt-4">
