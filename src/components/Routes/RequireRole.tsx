3c042693b3f632d5ce1786e109a3004a2986074c
Edgar Perez <edgarjoel9912@gmail.com>
2025-08-17 14:43:44 -0400

    feat(ui): Agregue el spinner en RequireRole

--- src/components/Routes/RequireRole.tsx
+++ src/components/Routes/RequireRole.tsx
@@ -1,8 +1,9 @@
 // components/Routes/RequireRole.tsx
-import { Navigate } from "react-router-dom";
-import { useAuth } from "../../context/AuthContext";
-import type { RoleName } from "../../services/userService";
-import type { JSX } from "react";
+import { Navigate } from 'react-router-dom';
+import { useAuth } from '../../context/AuthContext';
+import type { RoleName } from '../../services/userService';
+import type { JSX } from 'react';
+import Spinner from '../ui/Spinner';
 
 type RequireRoleProps = {
   allow: RoleName[];
@@ -12,7 +13,12 @@ type RequireRoleProps = {
 export default function RequireRole({ allow, children }: RequireRoleProps) {
   const { loading, isAuthenticated, role } = useAuth();
 
-  if (loading) return null; // o spinner
+  if (loading)
+    return (
+      <div className="h-screen w-screen grid place-items-center">
+        <Spinner />
+      </div>
+    );
   if (!isAuthenticated) return <Navigate to="/login" replace />;
   if (!role) return <Navigate to="/403" replace />;
 
